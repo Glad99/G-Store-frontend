1c7f69a05a89801f50a9b31d3a21c3f85a714406
Gladys <gladysalu1@gmail.com>
2024-04-24 18:40:37 +0100

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Footer from './Footer'
 
 const About = () => {
   return (
-    <div className='pt-[5rem]'>About</div>
+    <div className='pt-[5rem]'>
+      <div>
+        <div>
+          <h2>Our Story</h2>
+          <p>Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense in Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region. </p>
+          <p>Exclusive has more than 1 Million products to offer, growing at a very fast. Exclusive offers a diverse assotment in categories ranging  from consumer.</p>
+        </div>
+        <div>image</div>
+      </div>
+      <div>no of users</div>
+      <div>Founder, manager, product designer etc</div>
+      <Footer/>
+    </div>
   )
 }
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='pt-[5rem]'>Contact</div>
+    <div className='pt-[5rem]'>
+      <div>
+            <div>
+                <p>call icon</p>
+                <h3>Call To Us</h3>
+                <p>We are available 24/7, 7 days a week.</p>
+                <p>Phone: +2348106972329</p>
+            </div>
+            <hr />
+            <div>
+                <p>email box</p>
+                <h3>Write To Us</h3>
+            </div>
+            <p>Fill out our form and we will contact you within 24 hours.</p>
+            <p>Emails: Customer@g-Store.com </p>
+            <p>Emails: Support@g-Store.com </p>
+        </div>
+        <div>
+            <div>
+            <input type="text" placeholder='Your Name' />
+            <input type="text" placeholder='Your Email' />
+            <input type="text" placeholder='Your Phone' />
+            </div>
+            <input type="text" placeholder='You Message' />
+            <button>Send Message</button>
+        </div>
+    </div>
   )
 }
 
